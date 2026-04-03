eccdeafb6bc3aeab5bfe77b72dfcb14fd6ce5c5f
Dufffylaw <inna.solyakova@gmail.com>
2026-04-03 14:45:23 +0400

    test2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-inner">
-        <p>&copy; 2026 BookShelf. Учебный проект CI/CD.</p>
+        <p>&copy;Сломалось</p>
       </div>
     </footer>
   )
